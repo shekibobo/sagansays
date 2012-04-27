d6659e303d4f4f5e02aad995ff3cbe09ddba9378
Joshua Kovach <kovach.jc@gmail.com>
2012-04-27 00:32:28 -0400

    Add auth.js template without personal info

---
+++ auth_template.js
@@ -0,0 +1,12 @@
+var auth = 'auth+live+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+var userId = 'xxxxxxxxxxxxxxxxxxxxxxxx';
+var roomId = 'xxxxxxxxxxxxxxxxxxxxxxxx';
+
+
+var owner = '/Owner\ Name/';
+
+module.exports.auth = auth;
+module.exports.userId = userId;
+module.exports.roomId = roomId;
+
+module.exports.owner = owner;
